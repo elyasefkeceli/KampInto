12ddfff842038ec0fa6aafff45e610f7748450c6
elyase faruk keçeli <elyasefkeceli@gmail.com>
2021-03-23 17:23:12 +0300

    23 mart ders sonu

---
+++ GenericsInto/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericsInto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("elyase");
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
